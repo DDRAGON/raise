ea3073e3ac7fdbd5635658c16cc22a60acc88280
davide <davide.0829@hotmail.co.jp>
2015-01-06 01:04:05 +0900

    ビデオの横幅と縦幅を取得

--- public/javascripts/oddsSystem/video.js
+++ public/javascripts/oddsSystem/video.js
@@ -3,9 +3,16 @@ window.URL = window.URL || window.webkitURL;
 
 var video = document.getElementById('myVideo');
 var localStream = null;
+var videoWidth;
+var videoHeight;
+
 navigator.getUserMedia({video: true, audio: false},
 	function (stream) { // for success case
 		video.src = window.URL.createObjectURL(stream);
+		video.addEventListener('loadeddata', function() {
+			videoWidth  = video.videoWidth;
+			videoHeight = video.videoHeight;
+		}, false);
 	},
 	function (err) { // for error case
 		console.log(err);
