346cbaea768affa3699cf9bd6c6d16e52a7e3be0
davide <davide.0829@hotmail.co.jp>
2015-01-24 16:10:13 +0900

    ストラクチャーツールの作成中

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get('/oddsSystem', oddsSystem.oddsSystem);
 app.get('/qrCodeReader', qrCodeReader.qrCodeReader);
 app.get('/howToUse', justGet.justGet);
 app.get('/qrSheet', justGet.justGet);
+app.get('/structure', justGet.justGet);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
