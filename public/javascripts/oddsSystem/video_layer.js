559affa1b37ed75483e2cb61e419ef1dde1fa444
Davideryu Orihara <davideryu_orihara@dwango.co.jp>
2015-04-16 02:43:56 +0900

    今日やったところまで

--- public/javascripts/oddsSystem/video_layer.js
+++ public/javascripts/oddsSystem/video_layer.js
@@ -2,14 +2,21 @@ var DEFAULT_CANVAS_WIDTH  = 640;
 var DEFAULT_CANVAS_HEIGHT = 360;
 
 var canvasForVideo;
-var background = 'camera';
-var videoCutWidth  = 0;
-var videoCutHeight = 0;
-var flameWidth  = 0;
-var flameHeight = 0;
 var config = {
 	canvasWidth: DEFAULT_CANVAS_WIDTH,
-	canvasHeight: DEFAULT_CANVAS_HEIGHT
+	canvasHeight: DEFAULT_CANVAS_HEIGHT,
+	background: 'camera',
+	videoSourceX: 0,
+	videoSourceY: 0,
+	videoSourceWidth: DEFAULT_CANVAS_WIDTH,
+	videoSourceHeight: DEFAULT_CANVAS_HEIGHT,
+	videoDisplayX: 0,
+	videoDisplayY: 0,
+	videoDisplayWidth: DEFAULT_CANVAS_WIDTH,
+	videoDisplayHeight: DEFAULT_CANVAS_HEIGHT,
+	adjustToWindowResize: false,
+	isFullScreenMode: false,
+	isFullScreen169Mode: false
 };
 
 $("#changeScreenSize").change(function(){
@@ -20,68 +27,155 @@ $("#changeScreenSize").change(function(){
 		case 'camera':
 			setCameraSize();
 			break;
+		case 'fullScreen':
+			fullScreenMode();
+			break;
+		case '16:9_fullScreen':
+			fullScreen169Mode();
+			break;
 	}
 	setLayoutRound();
 });
 
 $("#changeBackground").change(function(){
-	background = $(this).val();
-	if (background === 'RGB(255,255,0)') {
+	config.background = $(this).val();
+	if (config.background === 'RGB(255,255,0)') {
 		$('#canvas_pane').attr('class', 'wall_yellow');
 	} else {
-		$('#canvas_pane').attr('class', 'wall_'+background);
+		$('#canvas_pane').attr('class', 'wall_'+config.background);
 	}
 });
 
 // ビデオの描画
 setInterval(function(){
 	if (!config.ctxForVideo) return;
-	if (background === 'camera') {
+	if (config.background === 'camera') {
 		// 背景カメラモードならカメラを描画
-		config.ctxForVideo.drawImage(
-			video,
-			videoCutWidth,
-			videoCutHeight,
-			config.canvasWidth,
-			config.canvasHeight,
-			flameWidth,
-			flameHeight,
-			config.canvasWidth,
-			config.canvasHeight
+		redrawCamera(
+			config.videoSourceX, config.videoSourceY,
+			config.videoSourceWidth, config.videoSourceHeight,
+			config.videoDisplayX, config.videoDisplayY,
+			config.videoDisplayWidth, config.videoDisplayHeight
 		);
-		return;
 	}
 }, 50);
 
+function redrawCamera(sx,sy, sw,sh, dx,dy, dw,dh) {
+	config.ctxForVideo.drawImage(video, sx,sy, sw,sh, dx,dy, dw,dh);
+}
+
 function setDefaultSize() {
-	config.canvasWidth    = DEFAULT_CANVAS_WIDTH;
-	config.canvasHeight   = DEFAULT_CANVAS_HEIGHT;
 	canvasForVideo.width  = DEFAULT_CANVAS_WIDTH;
 	canvasForVideo.height = DEFAULT_CANVAS_HEIGHT;
-	videoCutWidth  = parseInt((videoWidth  - config.canvasWidth) / 2);
-	videoCutHeight = parseInt((videoHeight - config.canvasHeight) / 2);
-	if (videoCutWidth < 0) { // ビデオがデフォルトサイズより小さい時
-		flameWidth  = Math.abs(videoCutWidth);
-		videoCutWidth  = 0;
+	config.canvasWidth  = DEFAULT_CANVAS_WIDTH;
+	config.canvasHeight = DEFAULT_CANVAS_HEIGHT;
+	config.videoSourceX = parseInt((videoWidth  - config.canvasWidth) / 2);
+	config.videoSourceY = parseInt((videoHeight - config.canvasHeight) / 2);
+	config.videoSourceWidth  = DEFAULT_CANVAS_WIDTH;
+	config.videoSourceHeight = DEFAULT_CANVAS_HEIGHT;
+	config.videoDisplayX = 0;
+	config.videoDisplayY = 0;
+	config.videoDisplayWidth  = DEFAULT_CANVAS_WIDTH;
+	config.videoDisplayHeight = DEFAULT_CANVAS_HEIGHT;
+
+	if (config.videoSourceX < 0) { // ビデオ横幅がデフォルト横幅より小さい時
+		config.videoSourceX = 0;
+		config.videoSourceWidth = videoWidth;
+		config.videoDisplayX = Math.abs(config.videoSourceX);
+		config.videoDisplayWidth = videoWidth;
 	}
-	if (videoCutHeight < 0) { // ビデオがデフォルトサイズより小さい時
-		flameHeight = Math.abs(videoCutHeight);
-		videoCutHeight = 0;
+	if (config.videoSourceY < 0) { // ビデオ縦幅がデフォルト縦幅より小さい時
+		config.videoSourceY = 0;
+		config.videoSourceHeight = videoHeight;
+		config.videoDisplayY = Math.abs(config.videoSourceY);
+		config.videoDisplayHeight = videoHeight;
 	}
+
+	$('#canvas_pane').css({width: config.canvasWidth + 'px', height: config.canvasHeight + 'px'});
 }
 
 function setCameraSize() {
-	config.canvasWidth    = videoWidth;
-	config.canvasHeight   = videoHeight;
 	canvasForVideo.width  = videoWidth;
 	canvasForVideo.height = videoHeight;
-	flameWidth  = 0;
-	flameHeight = 0;
-	videoCutWidth  = 0;
-	videoCutHeight = 0;
+	config.canvasWidth    = videoWidth;
+	config.canvasHeight   = videoHeight;
+	config.videoSourceX = 0;
+	config.videoSourceY = 0;
+	config.videoSourceWidth  = videoWidth;
+	config.videoSourceHeight = videoHeight;
+	config.videoDisplayX = 0;
+	config.videoDisplayY = 0;
+	config.videoDisplayWidth  = videoWidth;
+	config.videoDisplayHeight = videoHeight;
+	$('#canvas_pane').css({width: config.canvasWidth + 'px', height: config.canvasHeight + 'px'});
 }
 
+function fullScreenMode() {
+	// フラグの設定
+	config.adjustToWindowResize = true;
+	config.isFullScreenMode     = true;
+	config.isFullScreen169Mode  = false;
+
+	// 他の要素を隠す
+	$('#settings').hide();
+	$('.navbar-wrapper').hide();
+	$('#footer_contents').hide();
+
+	// キャンバスサイズやカメラサイズ設定
+	setSizeToFullScreen();
+}
+
+function fullScreen169Mode() {
+	// フラグの設定
+	config.adjustToWindowResize = true;
+	config.isFullScreenMode     = false;
+	config.isFullScreen169Mode  = true;
+
+	// 他の要素を隠す
+	$('#settings').hide();
+	$('.navbar-wrapper').hide();
+	$('#footer_contents').hide();
+
+	// キャンバスサイズやカメラサイズ設定
+	setSizeToFullScreen();
+}
+
+function setSizeToFullScreen() {
+	var windowWidth  = $(window).width();
+	var windowHeight = $(window).height();
+	// キャンバスサイズやカメラサイズ設定
+	canvasForVideo.width  = windowWidth;
+	canvasForVideo.height = windowHeight;
+	config.canvasWidth    = windowWidth;
+	config.canvasHeight   = windowHeight;
+	config.videoSourceX = 0;
+	config.videoSourceY = 0;
+	config.videoSourceWidth  = windowWidth;
+	config.videoSourceHeight = windowHeight;
+	config.videoDisplayX = 0;
+	config.videoDisplayY = 0;
+	config.videoDisplayWidth  = windowWidth;
+	config.videoDisplayHeight = windowHeight;
+	$('#canvas_pane').css({width: config.canvasWidth + 'px', height: config.canvasHeight + 'px'});
+}
+
+$(window).resize(function() {
+	if (config.adjustToWindowResize === false) return;
+	console.log('resized');
+	if (config.isFullScreenMode === true) { // フルスクリーンモードなら
+		setSizeToFullScreen(); // フルスクリーンサイズに合わせる。
+		setLayoutRound();
+		redrawCamera(
+			config.videoSourceX, config.videoSourceY,
+			config.videoSourceWidth, config.videoSourceHeight,
+			config.videoDisplayX, config.videoDisplayY,
+			config.videoDisplayWidth, config.videoDisplayHeight
+		);
+	}
+});
+
 $(function(){
+	$('.container').css({"width": 'auto', "padding-left": "0px", "padding-right": "0px"});
 	canvasForVideo = $('#canvasForVideo').get(0);
 	canvasForVideo.width  = DEFAULT_CANVAS_WIDTH;
 	canvasForVideo.height = DEFAULT_CANVAS_HEIGHT;
